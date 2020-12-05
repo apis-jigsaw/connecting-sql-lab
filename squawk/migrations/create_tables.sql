9231c9c48021f0258f2020d76d49897069c8d08a
Jeff Katz <JeffreyEricKatz@gmail.com>
2020-12-04 20:13:07 -0500

    update

---
+++ squawk/migrations/create_tables.sql
@@ -0,0 +1,17 @@
+DROP TABLE IF EXISTS restaurants;
+
+CREATE TABLE IF NOT EXISTS restaurants (
+  id serial PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  address VARCHAR(255),
+  city VARCHAR(255),
+  category VARCHAR(255),
+  rating DECIMAL,
+  url VARCHAR(255),
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE INDEX IF NOT EXISTS restaurants_rating_index ON restaurants (rating);
+
+
+
